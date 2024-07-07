5c2d44a0c4f755f653f7c782bc3488c286b06572
Noel <1017417793@qq.com>
2024-07-07 23:23:35 +0800

    auto numbering script

---
+++ GRE/Vocabulary/auto_number.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+import sys
+
+n = len(sys.argv)
+ 
+if n != 2:
+    print("Pass which day you want to review as command line arg")
+    exit(1)
+
+filename = "Day%s.md"%(sys.argv[1])
+wfilename = "temp.md"
+
+f = open(filename, "r")
+wf = open(wfilename, "w")
+
+if not f:
+    print("File %s does not exist"%(filename))
+    exit(1)
+
+lineCount = 1
+
+while True:
+    line = f.readline()
+    if not line:
+        break
+    if len(line) < 2:
+        continue
+    tokens = line.split("|")
+    if len(tokens) < 4:
+        break
+    oldNo = tokens[1]
+    oldNo.strip()
+    try:
+        oldNo = int(oldNo)
+    except:
+        wf.write(line)
+        continue
+    else:
+        no = str(lineCount)
+        word = tokens[2]
+        trans = tokens[3]
+        wf.write("| %d | %s | %s |\n"%(lineCount, word, trans))
+        lineCount += 1
+        
+f.close()
+wf.close()
+
+exit(0)
